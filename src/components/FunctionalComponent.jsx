c5a8368d37045969f7c94bf56d0340c76f2b570b
smohanprasath <s.mohanprasath19478@gmail.com>
2022-09-05 21:35:39 +0530

    Learned States & Props on 05/09/2022

--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-const FunctionalComponent = () =>{
-    return (
-        <h1>I am from FunctionalComponent</h1>
-    )
-}
+const FunctionalComponent = (props) => {
+  const [count, setCount] = useState(0);
+  const [originalName, setName] =useState('Mohan')
+  var {name, age} = props;
+//
+  return (
+    <div>
+      {/* Learning State */}
+      <h1>This is State example</h1>
+      <button onClick={() => setCount(count + 1)}>Click me to add 1</button>
+      <button onClick={() => setCount(count - 1)}>Click me to reduce 1</button>
+      <h1>{count}</h1>
+      {/* Learning Props */}
+      <h1>This is Props example</h1>
+      <button onClick={() => setName(name)}>Click me to change name</button>
+      <p>I am {originalName} of {age}</p>
+    </div>
+  );
+};
 export default FunctionalComponent;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import FunctionalComponent from './components/FunctionalComponent';
 import ClassComponent from './components/ClassComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+var addValue=1;
 root.render(
   <React.StrictMode>
     <App />
     <h1>functional</h1>
-    <FunctionalComponent/>
+    <FunctionalComponent name={'prasath'} age={20} addValue={addValue}/>
     <h1>ClassComponent</h1>
     <ClassComponent/>
   </React.StrictMode>
